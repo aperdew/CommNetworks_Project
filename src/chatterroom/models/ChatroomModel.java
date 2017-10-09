ea879c9be3f246087e622b2e40a52caf091cecc5
aperdew <perdewaaron@gmail.com>
2017-10-09 12:01:44 -0500

    changed messages->messageList

--- src/chatterroom/models/ChatroomModel.java
+++ src/chatterroom/models/ChatroomModel.java
@@ -5,8 +5,36 @@ import java.util.List;
 import chatterroom.models.MessageModel;
 
 public class ChatroomModel {
-	private List<MessageModel> messages = new ArrayList(); 
+	private List<MessageModel> messageList = new ArrayList(); 
+	private String topic ="";
+	private int id =-1;
 	public ChatroomModel(){
 		
 	}; 
+	
+	public List<MessageModel> getMessageList(){
+		return messageList;
+	}
+	
+	public void setMessageList(List<MessageModel> messageList){
+		this.messageList = messageList;
+	}
+	
+	public String getTopic(){
+		return topic;
+	}
+	
+	public void setTopic(String topic){
+		this.topic = topic;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	
 }
